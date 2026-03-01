e05a6f18cc6df2b15377e84420cce78b2d33307f
M0rton45 <135593630+M0rton45@users.noreply.github.com>
2026-03-01 22:21:24 +0100

    polaczenie api

--- src/components/PokeF/index.tsx
+++ src/components/PokeF/index.tsx
@@ -1,16 +1,37 @@
 "use client";
 import { useState } from "react";
 import { PokeInput } from "../PokeI";
+import axios from "axios";
 
 
 export const PokeForm = () =>{
     const [pokemon, setPokemon] = useState<number>(0);
+    const [pokeData, setPokeData] = useState<any>(null);
 
     const [isFormValid, setIsFormValid] = useState(false);
- 
+    const [loading, setLoading] = useState(false);
+
     function choseBtn(){
-        console.log("chosen"+pokemon);        
+        console.log("chosen"+pokemon);
+        catchPokemon();
     }
+
+    const catchPokemon = async () =>{
+        setLoading(true);
+        try{
+            // wywołanie api
+            // w .data są dane 
+            const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+            setPokeData(response.data);
+            console.log("Dane z API: ",response.data);
+            console.log("Dane z API: ",response.data.name);
+
+        }catch(error){
+            console.log(error);
+        }finally{
+            setLoading(false);
+        }
+    } 
 
     return(
     <>
